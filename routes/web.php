f412658b5154e70661fa8c3003b937cc16234deb
masato5579 <73378197+masato5579@users.noreply.github.com>
2021-12-18 20:03:08 +0900

    [add]Laravel Breeze インストール

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
