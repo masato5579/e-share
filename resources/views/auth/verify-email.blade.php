e2aafc94cbb56f2f5052911d49267c533004ef82
masato5579 <73378197+masato5579@users.noreply.github.com>
2021-12-19 12:48:23 +0900

    [fix]verify-email.blade.php不要部分削除

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,22 +1,14 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="">
             {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
+            <div class="">
                 {{ __('A new verification link has been sent to the email address you provided during registration.') }}
             </div>
         @endif
 
-        <div class="mt-4 flex items-center justify-between">
+        <div class="">
             <form method="POST" action="{{ route('verification.send') }}">
                 @csrf
 
@@ -30,10 +22,8 @@
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
 
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
+                <button type="submit" class="">
                     {{ __('Log Out') }}
                 </button>
             </form>
         </div>
-    </x-auth-card>
-</x-guest-layout>
