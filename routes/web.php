2707f5e62bdb682ffd4d96da286a7d3a72eeb552
masato5579 <73378197+masato5579@users.noreply.github.com>
2022-01-08 14:54:14 +0900

    [fix]/homeに適正なnameをつけて保守性をあげる

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', function () {
     return view('home');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth'])->name('home.index');
 
 require __DIR__ . '/auth.php';
